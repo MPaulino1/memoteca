37c04e8e0bf6704caba5700c53548619437ad7c1
Manoel Paulino <mpaulino.me@gmail.com>
2022-12-06 15:33:12 -0300

    up

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
+  listaPensamentos = [
+    {
+        conteudo: 'Angular',
+        autoria: 'Didier',
+        modelo: 'modelo3'
+    },
+    {
+        conteudo: 'Como minha empresa trabalha com serviços, as descrições dos trabalhos (produtos) são frequentemente extensas. Na minha instalação atual do compiere, uso o campo descrição das linhas de ordem de venda (e fatura) para esse objetivo, porém diversas vezes tive dificuldades com o número máximo de caracteres (512). Vendo o BD do adempiere, percebo que o limite desse mesmo campo é ainda menor (256 caracteres). Há algum problema em aumentar esses campos pra algo como 1000 caracteres? E aonde mudar isso? Preciso ir tabela por tabela como SuperUser, aumentar o limite e mandar sincronizar a coluna? Ou há uma maneira mais direta?',
+        autoria: 'Neymar',
+        modelo: 'modelo1'
+    }
+  ];
+
 }
